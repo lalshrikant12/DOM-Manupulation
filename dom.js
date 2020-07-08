22bb8f2bd3d5147fd19db81df75d287be5e546aa
Shri Kant Lal <46701610+lalshrikant12@users.noreply.github.com>
2020-07-08 23:49:13 +0530

    Create dom.js

---
+++ dom.js
@@ -0,0 +1,16 @@
+var body = document.querySelector("body"); //SELECT
+var isBlue =false;
+ setInterval(function())
+ {
+ 	if(isBlue)
+ 	{
+ 	body.style.background ="white";
+
+ 	}
+ 	else 
+ 	{
+ 	bodu.style.background="#3498db";
+ 	}
+ 	isBlue=!isBlue;
+
+ }, 100)
